33b097229af7c470bf54739dbcd488fa3eeec65e
user <user@users-MacBook-Air.local>
2018-10-16 23:28:21 +0700

    service fix

--- prj-services-final-4/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ prj-services-final-4/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { ShoppingListService } from '../../shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,12 +12,13 @@ import { RecipeService } from '../recipe.service';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private shop:ShoppingListService) { }
 
   ngOnInit() {
   }
 
   onAddToShoppingList() {
+    // this.shop.addIngredients(this.recipe.ingredients);
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 
