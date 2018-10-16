4c974b378f73ce28b0547af41f4a0a77a8851c70
user <user@users-MacBook-Air.local>
2018-10-17 06:30:28 +0700

    service observable

--- prj-routing-final-5 /src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ prj-routing-final-5 /src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { ActivatedRoute,Params ,Router} from '@angular/router' ;
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { ShoppingListService } from '../../shopping-list/shopping-list.service';
@@ -10,13 +10,21 @@ import { ShoppingListService } from '../../shopping-list/shopping-list.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
-
-  constructor(private recipeService: RecipeService, private shop:ShoppingListService) { }
+ recipe: Recipe;
+ id:number;
+  constructor(private router:Router,private recipeService: RecipeService, private shop:ShoppingListService,private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params:Params)=>{
+        this.id = + params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
+      }
+    )
   }
-
+  onEditRecipe(){
+    this.router.navigate(['edit'],{relativeTo:this.route});
+  }
   onAddToShoppingList() {
     // this.shop.addIngredients(this.recipe.ingredients);
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
