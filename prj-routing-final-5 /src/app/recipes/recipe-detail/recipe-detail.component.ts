4e41a5b3c68d3fca6fac290389e180b9a331633a
user <user@users-MacBook-Air.local>
2018-10-17 00:10:06 +0700

    routing

---
+++ prj-routing-final-5 /src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { ShoppingListService } from '../../shopping-list/shopping-list.service';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeDetailComponent implements OnInit {
+  @Input() recipe: Recipe;
+
+  constructor(private recipeService: RecipeService, private shop:ShoppingListService) { }
+
+  ngOnInit() {
+  }
+
+  onAddToShoppingList() {
+    // this.shop.addIngredients(this.recipe.ingredients);
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+}
