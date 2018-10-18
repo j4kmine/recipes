3d4d227a40db45eb19bcb48139fc0694fe11f18a
user <user@users-MacBook-Air.local>
2018-10-19 02:05:25 +0700

    http client

---
+++ prj-httpclient-final-10/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app.routing.module';
+import{ShoppingListModule} from './shopping-list/shopping-list.module';
+import {SharedModule} from './shared/shared.module';
+import {AuthModule} from './auth/auth.module';
+import {CoreModule}from './core/core.module';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ShoppingListModule, 
+    AuthModule,
+    CoreModule,
+    AppRoutingModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
