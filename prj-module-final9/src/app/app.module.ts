36087f95cf6dba133ba6abea91075970dad2e0e5
user <user@users-MacBook-Air.local>
2018-10-18 22:28:27 +0700

    preload

--- prj-module-final9/src/app/app.module.ts
+++ prj-module-final9/src/app/app.module.ts
@@ -3,12 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app.routing.module';
-import {RecipeService} from './recipes/recipe.service';
-import {DataStorageService} from './shared/data-storage.service';
-import {AuthService} from './auth/auth.service';
-import {AuthGuard}from './auth/auth.guard.service';
 import{ShoppingListModule} from './shopping-list/shopping-list.module';
 import {SharedModule} from './shared/shared.module';
 import {AuthModule} from './auth/auth.module';
@@ -26,7 +21,7 @@ import {CoreModule}from './core/core.module';
     AppRoutingModule,
     SharedModule
   ],
-  providers: [ShoppingListService,RecipeService,DataStorageService,AuthService,AuthGuard],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
